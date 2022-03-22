15765b6b1692f4151d5a19257be550dd5a1244e8
dzmitryhil <dzmitryhil@gmail.com>
2022-03-22 12:22:10 +0400

    change orchestrator block delay to 35

--- orchestrator/orchestrator/src/ethereum_event_watcher.rs
+++ orchestrator/orchestrator/src/ethereum_event_watcher.rs
@@ -17,6 +17,10 @@ use gravity_utils::{
 use metrics_exporter::metrics_errors_counter;
 use tonic::transport::Channel;
 
+const BLOCK_DELAY: u8 = 35;
+const LOCAL_GETH_CHAIN_ID: u64 = 15;
+const LOCAL_HARDHAT_CHAIN_ID: u64 = 31337;
+
 pub struct CheckedNonces {
     pub block_number: Uint256,
     pub event_nonce: Uint256,
@@ -31,16 +35,15 @@ pub async fn check_for_events(
     our_private_key: CosmosPrivateKey,
     fee: Coin,
     starting_block: Uint256,
-    block_delay: Uint256,
 ) -> Result<CheckedNonces, GravityError> {
     let our_cosmos_address = our_private_key.to_address(&contact.get_prefix()).unwrap();
     let latest_block = get_block_number_with_retry(web3).await;
-    let latest_block = latest_block - block_delay;
+    let latest_block_with_delay = latest_block - get_block_delay(web3).await;
 
     let deposits = web3
         .check_for_events(
             starting_block.clone(),
-            Some(latest_block.clone()),
+            Some(latest_block_with_delay.clone()),
             vec![gravity_contract_address],
             vec![SENT_TO_COSMOS_EVENT_SIG],
         )
@@ -50,7 +53,7 @@ pub async fn check_for_events(
     let batches = web3
         .check_for_events(
             starting_block.clone(),
-            Some(latest_block.clone()),
+            Some(latest_block_with_delay.clone()),
             vec![gravity_contract_address],
             vec![TRANSACTION_BATCH_EXECUTED_EVENT_SIG],
         )
@@ -60,7 +63,7 @@ pub async fn check_for_events(
     let valsets = web3
         .check_for_events(
             starting_block.clone(),
-            Some(latest_block.clone()),
+            Some(latest_block_with_delay.clone()),
             vec![gravity_contract_address],
             vec![VALSET_UPDATED_EVENT_SIG],
         )
@@ -70,7 +73,7 @@ pub async fn check_for_events(
     let erc20_deployed = web3
         .check_for_events(
             starting_block.clone(),
-            Some(latest_block.clone()),
+            Some(latest_block_with_delay.clone()),
             vec![gravity_contract_address],
             vec![ERC20_DEPLOYED_EVENT_SIG],
         )
@@ -80,7 +83,7 @@ pub async fn check_for_events(
     let logic_call_executed = web3
         .check_for_events(
             starting_block.clone(),
-            Some(latest_block.clone()),
+            Some(latest_block_with_delay.clone()),
             vec![gravity_contract_address],
             vec![LOGIC_CALL_EVENT_SIG],
         )
@@ -205,7 +208,7 @@ pub async fn check_for_events(
             }
         }
         Ok(CheckedNonces {
-            block_number: latest_block,
+            block_number: latest_block_with_delay,
             event_nonce: new_event_nonce,
         })
     } else {
@@ -232,32 +235,13 @@ pub async fn check_for_events(
 /// As you can see on https://etherscan.io/blocks_forked uncles (one block deep reorgs)
 /// occur once every few minutes. Two deep once or twice a day.
 /// https://etherscan.io/chart/uncles
-/// Let's make a conservative assumption of 1% chance of an uncle being a two block deep reorg
-/// (actual is closer to 0.3%) and assume that continues as we increase the depth.
-/// Given an uncle every 2.8 minutes, a 6 deep reorg would be 2.8 minutes * (100^4) or one
-/// 6 deep reorg every 53,272 years.
-///
-/// Of course the above assume that no mining attacks occur. Once we bring that potential into
-/// the equation the question becomes 'how much money'. There is no depth safe from infinite
-/// spending. Taking some source values from https://blog.ethereum.org/2016/05/09/on-settlement-finality/
-/// we will use 13 blocks providing a 1/1_000_000 chance of an attacker with 25% of network hash
-/// power succeeding
-///
-pub async fn get_block_delay(web3: &Web3) -> Uint256 {
+/// We use block delay of 35, giving preference to security over speed.
+async fn get_block_delay(web3: &Web3) -> Uint256 {
     let net_version = get_net_version_with_retry(web3).await;
 
     match net_version {
-        // Mainline Ethereum, Ethereum classic, or the Ropsten, Kotti, Mordor testnets
-        // all POW Chains
-        1 | 3 | 6 | 7 => 13u8.into(),
-        // Dev, our own Gravity Ethereum testnet, and Hardhat respectively
-        // all single signer chains with no chance of any reorgs
-        2018 | 15 | 31337 => 0u8.into(),
-        // Rinkeby and Goerli use Clique (POA) Consensus, finality takes
-        // up to num validators blocks. Number is higher than Ethereum based
-        // on experience with operational issues
-        4 | 5 => 10u8.into(),
-        // assume the safe option (POW) where we don't know
-        _ => 13u8.into(),
+        // For the chains we use for the integration tests we don't require the block delay.
+        LOCAL_GETH_CHAIN_ID | LOCAL_HARDHAT_CHAIN_ID => 0u8.into(),
+        _ => BLOCK_DELAY.into(),
     }
 }
