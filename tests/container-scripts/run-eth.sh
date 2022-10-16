437f4197f3b91ca858c7387ad12583c6186eeb0b
Aaron Kutch <aaronkutch@att.net>
2022-10-16 18:46:39 -0500

    add additional sync wait loop

--- tests/container-scripts/run-eth.sh
+++ tests/container-scripts/run-eth.sh
@@ -45,8 +45,12 @@ else
         --staking-enabled=false &> /gravity/tests/assets/avalanchego.log &
 
     echo "waiting for avalanche to come online"
-    until $(curl --output /dev/null --fail --silent --header "content-type: application/json" --data '{"method":"eth_blockNumber","params":[],"id":93,"jsonrpc":"2.0"}' http://localhost:8545/ext/bc/C/rpc); do
+    until $(curl --output /dev/null --fail --silent --header "content-type: application/json" --data '{"method":"eth_blockNumber","params":[],"id":1,"jsonrpc":"2.0"}' http://localhost:8545/ext/bc/C/rpc); do
         printf '.'
+        sleep 1
+    done
+    echo "waiting for avalanche to sync"
+    until [ "$(curl -s --header "content-type: application/json" --data '{"id":1,"jsonrpc":"2.0","method":"eth_syncing","params":[]}' http://localhost:8545/ext/bc/C/rpc)" == '{"jsonrpc":"2.0","id":1,"result":false}' ]; do
         sleep 1
     done
 fi
