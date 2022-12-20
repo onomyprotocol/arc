0c36514bb2c762d87ce4d36bb97b2b639b551e02
Aaron Kutch <aaronkutch@att.net>
2022-12-20 12:02:13 -0600

    Port to Avax, `avalanchego` v1.9.4

--- tests/container-scripts/run-eth.sh
+++ tests/container-scripts/run-eth.sh
@@ -33,32 +33,25 @@ elif [[ ! -z "$HARDHAT" ]]; then
 # the right number of cpu cores and Geth goes crazy consuming all the processing power, on the other hand
 # hardhat doesn't work for some tests that depend on transactions waiting for blocks, so Geth is the default
 else
-    # init the genesis block
-    geth --identity "GravityTestnet" \
-        --nodiscover \
-        --networkid 15 \
-        init /gravity/tests/assets/ETHGenesis.json
+    # so that database related folders are not spawning in the scripts folder
+    pushd /
+    avalanchego \
+        --genesis="/gravity/tests/assets/ETHGenesis.json" \
+        --chain-config-dir="/gravity/tests/assets/avalanchego_configs" \
+        --network-id=15 \
+        --build-dir="/avalanchego/build/" \
+        --public-ip=127.0.0.1 \
+        --http-port=8545 \
+        --db-type=memdb \
+        --staking-enabled=false &> /gravity/tests/assets/avalanchego.log &
 
-    # etherbase is where rewards get sent
-    # private key for this address is 0xb1bab011e03a9862664706fc3bbaa1b16651528e5f0e7fbfcbfdd8be302a13e7
-    geth --identity "GravityTestnet" --nodiscover \
-        --networkid 15 \
-        --mine \
-        --http \
-        --http.addr="0.0.0.0" \
-        --http.vhosts="*" \
-        --http.corsdomain="*" \
-        --miner.threads=1 \
-        --nousb \
-        --verbosity=5 \
-        --miner.etherbase=0xBf660843528035a5A4921534E156a27e64B231fE &> /gravity/tests/assets/geth.log &
-
-    echo "waiting for geth to come online"
-    until $(curl --output /dev/null --fail --silent --header "content-type: application/json" --data '{"method":"eth_blockNumber","params":[],"id":1,"jsonrpc":"2.0"}' http://localhost:8545); do
+    echo "waiting for avalanche to come online"
+    until $(curl --output /dev/null --fail --silent --header "content-type: application/json" --data '{"method":"eth_blockNumber","params":[],"id":1,"jsonrpc":"2.0"}' http://localhost:8545/ext/bc/C/rpc); do
+        printf '.'
         sleep 1
     done
-    echo "waiting for geth to sync"
-    until [ "$(curl -s --header "content-type: application/json" --data '{"id":1,"jsonrpc":"2.0","method":"eth_syncing","params":[]}' http://localhost:8545)" == '{"jsonrpc":"2.0","id":1,"result":false}' ]; do
+    echo "waiting for avalanche to sync"
+    until [ "$(curl -s --header "content-type: application/json" --data '{"id":1,"jsonrpc":"2.0","method":"eth_syncing","params":[]}' http://localhost:8545/ext/bc/C/rpc)" == '{"jsonrpc":"2.0","id":1,"result":false}' ]; do
         sleep 1
     done
 fi
